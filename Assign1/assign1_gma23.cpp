0a7bbc9d0c4cdfbd3bc9f116d3176e0cfee9d040
Gentry Atkinson <gma23@txstate.edu>
2019-01-28 12:12:49 -0600

    completed first assignment

---
+++ Assign1/assign1_gma23.cpp
@@ -0,0 +1,19 @@
+//File Name: assign1_gma23.cpp
+//Author: Gentry Atkinson
+//Date: 1/24/2019
+//Assignment Number: 1
+//CS 1428.253 Spring 2019
+//Instructor: Jill Seaman
+//Output information about the author to the command line.
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "Name: Gentry Atkinson" << endl << endl;
+    cout << "Major: Computer Science" << endl << endl;
+    cout << "Favorite Movie: SLC Punk! 1998" << endl;
+
+    return 0;
+}
